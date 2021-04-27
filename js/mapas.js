b1bad5856a5cc882c89feae03eee393f1600951c
Manuel Vargas <mfvargas@gmail.com>
2021-04-26 23:05:35 -0600

    Update mapas.js

--- js/mapas.js
+++ js/mapas.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([10, -84], 13);
 
 L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', 
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', 
   {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
 ).addTo(mymap);
